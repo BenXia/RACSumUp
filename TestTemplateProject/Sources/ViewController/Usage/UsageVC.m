b10656da5881bfad37a8e6b8bee45e480e890b5c
夏许强 <xiaxuqiang@changingedu.com>
2019-08-01 15:10:22 +0800

    添加 takeUntilBlock 使用 demo

--- TestTemplateProject/Sources/ViewController/Usage/UsageVC.m
+++ TestTemplateProject/Sources/ViewController/Usage/UsageVC.m
@@ -17,6 +17,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [[[self sig_simple] takeUntilBlock:^BOOL(id x) {
+        int indexNum = [x intValue];
+        
+        if (indexNum == 3) {
+            // Flag: 此处可以插入停止时需要做的事情
+            NSLog(@"此处可以插入停止时需要做的事情");
+            
+            return YES;
+        }
+        
+        return NO;
+    }] subscribeNext:^(id x) {
+        // need do nothing, 要做的在takeUntilBlock:中做完了
+        
+        NSLog(@"get next: %@", x);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +41,19 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (RACSignal *)sig_simple {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        
+        for (int i = 1; i < 6; i++) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [subscriber sendNext:@(i)];
+            });
+        }
+        
+        return nil;
+    }];
 }
-*/
 
 @end
+
+
