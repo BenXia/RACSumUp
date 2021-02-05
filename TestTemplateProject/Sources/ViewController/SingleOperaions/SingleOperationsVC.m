2b8f2976bd9ec2f33c8fc942b0bd72a76be6aff0
夏许强 <xiaxuqiang@changingedu.com>
2021-02-05 11:22:41 +0800

    添加 throttle 和 distinctUntilChanged 组合使用的测试

--- TestTemplateProject/Sources/ViewController/SingleOperaions/SingleOperationsVC.m
+++ TestTemplateProject/Sources/ViewController/SingleOperaions/SingleOperationsVC.m
@@ -10,6 +10,8 @@
 
 @interface SingleOperationsVC ()
 
+@property (nonatomic, assign) BOOL isEnter;
+
 @end
 
 @implementation SingleOperationsVC
@@ -17,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    [self testThrottleOperation];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +28,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)testThrottleOperation {
+    [[[RACObserve(self, isEnter) distinctUntilChanged] throttle:5] subscribeNext:^(id  _Nullable x) {
+        NSLog(@"get next value: %@", x);
+    } error:^(NSError * _Nullable error) {
+        NSLog(@"get error: %@", error);
+    }];
+    
+//    [[[RACObserve(self, isEnter) throttle:5] distinctUntilChanged] subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"get next value: %@", x);
+//    } error:^(NSError * _Nullable error) {
+//        NSLog(@"get error: %@", error);
+//    }];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.isEnter = YES;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.isEnter = NO;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.isEnter = YES;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.isEnter = YES;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(9 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.isEnter = NO;
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.isEnter = YES;
+    });
 }
-*/
 
 @end
